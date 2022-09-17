1077c527af207583e59bc3654220e92f596a11ba
Abathur Izetta <aizetta@oa-n2.msk.21-school.ru>
2022-09-17 19:45:51 +0300

    1

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,12 @@ Server::Server(int port, std::string pass): port(port), password(pass) {
     //TODO: init params here
 }
 Server::~Server(){
+    std::string		line;
+    std::ifstream	jsonFile(CONFIG);
+    if (!jsonFile.is_open())
+    {
+        std::cout << "ERROR OPEN JSON FILE" << std::endl;
+    }
     loadConfig("CONFIG");
     //TODO: check if we alloced something and delete this shit if any
 }
@@ -13,6 +19,6 @@ Server::~Server(){
 //returns true on success
 //private bool read_config(std::string configAddress)
 //exit with error on false
-void Server::loadConfig(std::string configAddress) {
-
+bool Server::loadConfig(std::string configAddress) {
+    std::cout << "ZBS" << std::endl;
 }
