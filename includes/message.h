0f0363d704589538177fb3ab30249b9a17ea5d49
Carly Yuriko <cyuriko@oa-m1.msk.21-school.ru>
2022-11-06 13:30:04 +0300

    some tasks

---
+++ includes/message.h
@@ -0,0 +1,14 @@
+//
+// Created by Carly Yuriko on 11/5/22.
+//
+
+#ifndef FT_IRC_MESSAGE_H
+#define FT_IRC_MESSAGE_H
+
+
+class message {
+
+};
+
+
+#endif //FT_IRC_MESSAGE_H
---
+++ src/message.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carly Yuriko on 11/5/22.
+//
+
+#include "message.h"
